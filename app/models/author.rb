27546748e2050587f51196a0b6ff2c0225205c51
Makoto Inomata <inomar1486@gmail.com>
2017-07-28 12:20:26 +0900

    test model author

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,5 +3,11 @@ class Author < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :pen_name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :password, length: { in: 6..18 }
+  validates :description, length: { maximum: 1000 }
+
   attachment :image
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Author, type: :model do
     expect(build(:author)).to be_valid
   end
 
+  # 有効である状態
   context "is valid" do
     # 名前とペンネームとメールがあれば有効な状態
     it "with a name, pen_name and email" do
@@ -19,21 +20,92 @@ RSpec.describe Author, type: :model do
     end
   end
 
+  # 無効である状態
   context "is invalid" do
     # 名前がなければ無効状態
-    it "without a name"
-    
+    it "without a name" do
+      author = build(:author, name: nil)
+      author.valid?
+      expect(author.errors[:name]).to include("を入力してください")
+    end
+
+    # 名前が最大文字数を越えれば無効
+    it "a name maximum number" do
+      max = 50
+      author = build(:author, name: "あ" * (max+1))
+      author.valid?
+      expect(author.errors[:name]).to include("は#{max}文字以内で入力してください")
+    end
+
     # ペンネームがなければ無効
-    it "without a pen_name"
+    it "without a pen_name" do
+      author = build(:author, pen_name: nil)
+      author.valid?
+      expect(author.errors[:pen_name]).to include("を入力してください")
+    end
+
+    # ペンネームが最大文字数を越えれば無効
+    it "a pen_name maximum number" do
+      max = 50
+      author = build(:author, pen_name: "あ" * (max+1))
+      author.valid?
+      expect(author.errors[:pen_name]).to include("は#{max}文字以内で入力してください")
+    end
+
+    # ペンネームが重複していれば無効
+    it "with a duplicate pen_name" do
+      create(:author, pen_name: "inomar")
+      author = build(:author, pen_name: "inomar")
+      author.valid?
+      expect(author.errors[:pen_name]).to include("はすでに存在します")
+    end
 
     # メールがなければ無効
-    it "without a email"
+    it "without a email" do
+      author = build(:author, email: nil)
+      author.valid?
+      expect(author.errors[:email]).to include("を入力してください")
+    end
+
+    # メールが重複していれば無効
+    it "with a duplicate email" do
+      create(:author, email: "inomar@example.com")
+      author = build(:author, email: "inomar@example.com")
+      author.valid?
+      expect(author.errors[:email]).to include("はすでに存在します")
+    end
 
     # パスワードがなければ無効
-    it "without a password"
+    it "without a password" do
+      author = build(:author, password: nil)
+      author.valid?
+      expect(author.errors[:password]).to include("を入力してください")
+    end
 
-    # 重複したメールアドレスなら無効状態であること
-    it "with a suplicate email"
+    # パスワードが最大文字数を越えれば無効
+    it "a password maximum number" do
+      max = 18
+      author = build(:author, password: "a" * (max+1))
+      author.valid?
+      expect(author.errors[:password]).to include("は#{max}文字以内で入力してください")
+    end
+
+    # パスワードが最小文字数より下回れば無効
+    it "a password maximum number" do
+      min = 6
+      author = build(:author, password: "a" * (min-1))
+      author.valid?
+      expect(author.errors[:password]).to include("は#{min}文字以上で入力してください")
+    end
+
+    # 説明が最大文字数を越えれば無効
+    it "a description maximum number" do
+      max = 1000
+      author = build(:author, description: "あ" * (max + 1))
+      author.valid?
+      expect(author.errors[:description]).to include("は#{max}文字以内で入力してください")
+    end
+
   end
 
 end
