143d419230d9b11b025766eec5b5dd8c8fcc2c98
Makoto Inomata <inomar1486@gmail.com>
2017-09-27 22:09:21 +0900

    speck favorite controller [pending

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe FavoritesController, type: :controller do
+	describe 'POST #create' do
+		context 'with params[:id]' do
+			it 'respond the :js'
+		end
+
+		context	'be current author' do
+
+		end
+
+		context 'without params[:id]' do
+			it 'respond the :js'
+		end
+
+		context	'not current author' do
+
+		end
+	end
+
+	describe	'POST #delete' do
+		context 'with params[:id]' do
+
+		end
+
+		context 'be current author' do
+
+		end
+
+		context 'without params[:id]' do
+
+		end
+
+		context 'not current author' do
+
+		end
+
+	end
 
 end
